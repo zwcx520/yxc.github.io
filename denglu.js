7a089404001af081984526c049a3b7e95f8bb875
追吻辰星 <1837601513@qq.com>
2025-04-21 05:53:28 +0800

    Update denglu.js

--- denglu.js
+++ denglu.js
@@ -10,7 +10,7 @@ let users = [
         const foundUser = users.find(user => user.username === username && user.password === password);
         if (foundUser) {
             // 登录成功后，使用window.location.href进行页面跳转，这里跳转到success.html页面，可按需修改
-            window.location.href = "https://lcy20.netlify.app/gd.html";
+            window.location.href = "https://yyxc.netlify.app/yxc.html";
         } else {
             errorElement.textContent = '账号或密码错误，请重新输入，临时账号只可使用一次！';
         }
